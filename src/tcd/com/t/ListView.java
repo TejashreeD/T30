02fbe320e82eaf0ae78c14f31b75fe5357a24717
TejashreeD <deshmukhtejashree1@gmail.com>
2013-10-04 20:36:52 +0530

    trying for the list and spinner

---
+++ src/tcd/com/t/ListView.java
@@ -0,0 +1,188 @@
+package tcd.com.t;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class ListView extends Activity implements OnItemSelectedListener {
+
+	JSONObject jsonResponse;
+	JSONArray jsonMainNode;
+	String email, pwd, item;
+	// Spinner Drop down elements
+	List<String> Classes = new ArrayList<String>();
+	List<String> list = new ArrayList<String>();
+ListView lists;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_list_view);
+		final Button GetServerData = (Button) findViewById(R.id.buttonTimatableGo);
+
+		Spinner spinner = (Spinner) findViewById(R.id.spinnerNew);
+				// Spinner click listener
+		spinner.setOnItemSelectedListener(this);
+
+		// Creating adapter for spinner
+		ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_spinner_item, Classes);
+		dataAdapter
+				.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		// attaching data adapter to spinner
+		spinner.setAdapter(dataAdapter);
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+	              android.R.layout.simple_list_item_1, android.R.id.text1, Classes);
+		
+		GetServerData.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				// Server Request URL
+				String serverURL = "http://115.111.105.152/schoolApp/allClasses";
+
+				// Create Object and call AsyncTask execute Method
+				new LongOperation().execute(serverURL);
+
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.list_view, menu);
+		return true;
+	}
+
+	// Class with extends AsyncTask class
+	private class LongOperation extends AsyncTask<String, Void, Void> {
+
+		private final HttpClient Client = new DefaultHttpClient();
+		private String Content;
+		private String Error = null;
+		private ProgressDialog Dialog = new ProgressDialog(ListView.this);
+
+		TextView uiUpdate = (TextView) findViewById(R.id.textView1);
+
+		protected void onPreExecute() {
+			// NOTE: You can call UI Element here.
+
+			// UI Element
+			uiUpdate.setText("Output : ");
+			Dialog.setMessage("Downloading source..");
+			Dialog.show();
+		}
+
+		// Call after onPreExecute method
+		protected Void doInBackground(String... urls) {
+			try {
+
+				// Call long running operations here (perform background
+				// computation)
+				// NOTE: Don't call UI Element here.
+
+				// Server url call by GET method
+				HttpGet httpget = new HttpGet(urls[0]);
+
+				ResponseHandler<String> responseHandler = new BasicResponseHandler();
+				Content = Client.execute(httpget, responseHandler);
+				/* tttttrrrryyyyyyyyyyyyyyyyyyy */
+
+				try {
+					jsonResponse = new JSONObject(Content);
+					jsonMainNode = jsonResponse.optJSONArray("ResultSet");
+					int lengthJsonArr = jsonMainNode.length();
+
+					for (int i = 0; i < lengthJsonArr; i++) {
+						System.out.println("OKAY_9!!");
+						/****** Get Object for each JSON node. ***********/
+						JSONObject jsonChildNode = jsonMainNode
+								.getJSONObject(i);
+
+						/******* Fetch node values **********/
+						email = jsonChildNode.optString("className").toString();
+						pwd = jsonChildNode.optString("classId").toString();
+						System.out.println(email + pwd + "\n\n\n");
+						//Classes.add(email);
+						Classes.add(jsonChildNode.optString("className").toString());
+						list.add(email);
+					}
+				} catch (JSONException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				/* tttttrrrryyyyyyyyyyyyyyyyyyy end */
+
+			} catch (ClientProtocolException e) {
+				Error = e.getMessage();
+				cancel(true);
+			} catch (IOException e) {
+				Error = e.getMessage();
+				cancel(true);
+			}
+
+			return null;
+		}
+
+		protected void onPostExecute(Void unused) {
+			// NOTE: You can call UI Element here.
+
+			// Close progress dialog
+			Dialog.dismiss();
+
+			if (Error != null) {
+
+				uiUpdate.setText("Output : " + Error);
+
+			} else {
+
+				uiUpdate.setText("Output : " + Content);
+
+			}
+		}
+
+	}
+
+	@Override
+	public void onItemSelected(AdapterView<?> parent, View view, int pos,
+			long id) {
+		// TODO Auto-generated method stub
+		item = parent.getItemAtPosition(pos).toString();
+		Toast.makeText(parent.getContext(), "Selected: " + item,
+				Toast.LENGTH_LONG).show();
+	}
+
+	@Override
+	public void onNothingSelected(AdapterView<?> arg0) {
+		// TODO Auto-generated method stub
+
+	}
+}
--- src/tcd/com/t/SpinnerJson.java
+++ src/tcd/com/t/SpinnerJson.java
@@ -10,12 +10,16 @@ import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.StatusLine;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.app.Activity;
 import android.util.Log;
 import android.view.Menu;
@@ -34,9 +38,9 @@ public class SpinnerJson extends Activity implements OnItemSelectedListener {
 	String readFeed;
 	JSONObject json;
 	Spinner mySpinner;
-	String spinnerElement;
+	String spinnerElement; String cnames[]=new String[10];
+	ResponseHandler<String> responseHandler = new BasicResponseHandler();
 
-	
 	// you can use this array to find the school ID based on name
 	List<Classes> Classes = new ArrayList<Classes>();
 	// you can use this array to populate your spinner
@@ -47,44 +51,48 @@ public class SpinnerJson extends Activity implements OnItemSelectedListener {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_spinner_json);
 		mySpinner = (Spinner) findViewById(R.id.spinnerNew);
-		/*
-		List<String> categories = new ArrayList<String>();
-		categories.add("Super Admn");
-		categories.add("Admn");
-		categories.add("Teacher");
-		categories.add("Parent");
-		// categories.add("Student");*/
-
-		readFeed();
+		
+		FeadFeed(cnames);
+		for (int i = 0; i < cnames.length; i++) {
+			System.out.print(cnames[i]+" add ok");
+			classNames.add(cnames[i]);}
 		mySpinner.setOnItemSelectedListener(this);
 		ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
 				android.R.layout.simple_spinner_item, classNames);
 
 		// Drop down layout style - list view with radio button
-		dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		dataAdapter
+				.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
 		// attaching data adapter to spinner
 		mySpinner.setAdapter(dataAdapter);
 
 	}
 
-	public void readFeed() {
-
-		new Thread() {
-
+	private void FeadFeed(final String[] cnames) {
+		// TODO Auto-generated method stub
+		Thread background = new Thread(new Runnable(){
 			public void run() {
 				// TODO Run network requests here.
+				String SetServerString;
 				HttpClient httpClient = new DefaultHttpClient();
+				
 				HttpGet get = new HttpGet(
 						"http://115.111.105.152/schoolApp/allClasses");
 				get.setHeader("content-type", "application/json; charset=UTF-8");
+				
 
 				try {
+					
 
+  ResponseHandler<String> responseHandler = new BasicResponseHandler();
+                 SetServerString = httpClient.execute(get, responseHandler);
+                 threadMsg(SetServerString);
 					System.out.println("OKAY_0!!");
 					HttpResponse resp = httpClient.execute(get);
+					String respStr = httpClient.execute(get,responseHandler);
 					System.out.println("OKAY_3!!" + resp);
-
+System.out.println(respStr+"Hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii");
 					StatusLine statusLine = resp.getStatusLine();
 					int statusCode = statusLine.getStatusCode();
 					if (statusCode == 200) {
@@ -127,21 +135,201 @@ public class SpinnerJson extends Activity implements OnItemSelectedListener {
 						Classes2.setName(jsonObject.optString("className"));
 						Classes2.setId(jsonObject.optString("classId"));
 						Classes.add(Classes2);
-						spinnerElement=jsonObject.optString("className");
-						System.out.println(" "+spinnerElement);
-						//classNames.add(jsonObject.optString("className"));
-								classNames.add(spinnerElement);
+						spinnerElement = jsonObject.optString("className");
+						System.out.println(" " + spinnerElement);
+						// classNames.add(jsonObject.optString("className"));
+						cnames[i]=spinnerElement;
+						
+						System.out.print(cnames[i]+" add elements ok");
+						
+						
 					}
+										/*for (int i = 0; i < jsonArray.length(); i++) {
+						System.out.print(cnames[i]+" add ok");
+						classNames.add(cnames[i]);}
+					*/
+					
+					
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 
 			}
-		}.start();
-		System.out.println("OKAY!!" + builder.toString());
 
+			private void threadMsg(String msg) {
+				// TODO Auto-generated method stub
+				if (!msg.equals(null) && !msg.equals("")) {
+                    Message msgObj = handler.obtainMessage();
+                    Bundle b = new Bundle();
+                    b.putString("message", msg);
+                    msgObj.setData(b);
+                    handler.sendMessage(msgObj);
+                }
+			
+			}
+
+			 private final Handler handler = new Handler() {
+				 
+                 public void handleMessage(Message msg) {
+                      
+                     String aResponse = msg.getData().getString("message");
+
+                     if ((null != aResponse)) {
+
+                         // ALERT MESSAGE
+                         Toast.makeText(
+                                 getBaseContext(),
+                                 "Server Response: "+aResponse,
+                                 Toast.LENGTH_SHORT).show();
+                     }
+                     else
+                     {
+
+                             // ALERT MESSAGE
+                             Toast.makeText(
+                                     getBaseContext(),
+                                     "Not Got Response From Server.",
+                                     Toast.LENGTH_SHORT).show();
+                     }   
+
+                 }
+             };
+		
+		});
+		background.start();
 		
 	}
+
+//	public void readFeed(cnames) {
+//
+//		Thread background = new Thread(new Runnable(){
+//			public void run() {
+//				// TODO Run network requests here.
+//				String SetServerString;
+//				HttpClient httpClient = new DefaultHttpClient();
+//				
+//				HttpGet get = new HttpGet(
+//						"http://115.111.105.152/schoolApp/allClasses");
+//				get.setHeader("content-type", "application/json; charset=UTF-8");
+//				
+//
+//				try {
+//					
+//
+//  ResponseHandler<String> responseHandler = new BasicResponseHandler();
+//                 SetServerString = httpClient.execute(get, responseHandler);
+//                 threadMsg(SetServerString);
+//					System.out.println("OKAY_0!!");
+//					HttpResponse resp = httpClient.execute(get);
+//					String respStr = httpClient.execute(get,responseHandler);
+//					System.out.println("OKAY_3!!" + resp);
+//System.out.println(respStr+"Hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii");
+//					StatusLine statusLine = resp.getStatusLine();
+//					int statusCode = statusLine.getStatusCode();
+//					if (statusCode == 200) {
+//						System.out.print("status code" + statusCode + "\n\n");
+//						HttpEntity entity = resp.getEntity();
+//
+//						InputStream content = entity.getContent();
+//						System.out.println("entiry=" + entity + "content="
+//								+ content);
+//						BufferedReader reader = new BufferedReader(
+//								new InputStreamReader(content));
+//						String line;
+//						while ((line = reader.readLine()) != null) {
+//							builder.append(line);
+//						}
+//					} else {
+//						Log.e(SpinnerJson.class.toString(),
+//								"Failed to download file");
+//					}
+//					System.out.println("OKAY_4!!");
+//					System.out.println("OKAY_5!!" + builder.toString());
+//
+//				} catch (Exception exception) {
+//					Log.e("MYAPP", "exception", exception);
+//				}
+//
+//				/* try */
+//				try {
+//					readFeed = builder.toString();
+//					json = new JSONObject(readFeed);
+//					JSONArray jsonArray = new JSONArray(
+//							json.optString("ResultSet"));
+//					Log.i(SpinnerJson.class.getName(), "Number of entries "
+//							+ jsonArray.length());
+//
+//					for (int i = 0; i < jsonArray.length(); i++) {
+//						JSONObject jsonObject = jsonArray.getJSONObject(i);
+//
+//						Classes Classes2 = new Classes();
+//						Classes2.setName(jsonObject.optString("className"));
+//						Classes2.setId(jsonObject.optString("classId"));
+//						Classes.add(Classes2);
+//						spinnerElement = jsonObject.optString("className");
+//						System.out.println(" " + spinnerElement);
+//						// classNames.add(jsonObject.optString("className"));
+//						cnames[i]=spinnerElement;
+//						
+//						System.out.print(cnames[i]+" add elements ok");
+//						
+//						
+//					}
+//										/*for (int i = 0; i < jsonArray.length(); i++) {
+//						System.out.print(cnames[i]+" add ok");
+//						classNames.add(cnames[i]);}
+//					*/
+//					
+//					
+//				} catch (Exception e) {
+//					e.printStackTrace();
+//				}
+//
+//			}
+//
+//			private void threadMsg(String msg) {
+//				// TODO Auto-generated method stub
+//				if (!msg.equals(null) && !msg.equals("")) {
+//                    Message msgObj = handler.obtainMessage();
+//                    Bundle b = new Bundle();
+//                    b.putString("message", msg);
+//                    msgObj.setData(b);
+//                    handler.sendMessage(msgObj);
+//                }
+//			
+//			}
+//
+//			 private final Handler handler = new Handler() {
+//				 
+//                 public void handleMessage(Message msg) {
+//                      
+//                     String aResponse = msg.getData().getString("message");
+//
+//                     if ((null != aResponse)) {
+//
+//                         // ALERT MESSAGE
+//                         Toast.makeText(
+//                                 getBaseContext(),
+//                                 "Server Response: "+aResponse,
+//                                 Toast.LENGTH_SHORT).show();
+//                     }
+//                     else
+//                     {
+//
+//                             // ALERT MESSAGE
+//                             Toast.makeText(
+//                                     getBaseContext(),
+//                                     "Not Got Response From Server.",
+//                                     Toast.LENGTH_SHORT).show();
+//                     }   
+//
+//                 }
+//             };
+//		
+//		});
+//		background.start();
+//
+//	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -154,15 +342,16 @@ public class SpinnerJson extends Activity implements OnItemSelectedListener {
 	public void onItemSelected(AdapterView<?> parent, View view, int position,
 			long id) {
 		// TODO Auto-generated method stub
-		 item = parent.getItemAtPosition(position).toString();
-		 Toast.makeText(parent.getContext(), "Selected: " + item,
-					Toast.LENGTH_LONG).show();
+		item = parent.getItemAtPosition(position).toString();
+		Toast.makeText(parent.getContext(), "Selected: " + item,
+				Toast.LENGTH_LONG).show();
 	}
 
 	@Override
-	public void onNothingSelected(AdapterView<?> arg0) {
+	public void onNothingSelected(AdapterView<?> parent) {
 		// TODO Auto-generated method stub
-
+		Toast.makeText(parent.getContext(), "Selected: " + item,
+				Toast.LENGTH_LONG).show();
 	}
 
 }
